8cf2c2c0cd7b09a7aebdbc63e69fcecad1bc2854
Musab <musab.k96@gmail.com>
2022-09-19 14:48:12 +0200

    Finish etch a sketch project

--- javascript.js
+++ javascript.js
@@ -7,22 +7,31 @@ const slider = document.querySelector("#sliderValue");
 const resetButton = document.querySelector(".reset-button");
 const eraserButton = document.querySelector(".eraser-button");
 const colorPicker = document.querySelector(".color-picker");
-canvasArea(slider.value);
-
+const rainbowButton = document.querySelector(".rainbow-button");
 
 let currentColor = colorPicker.value;
 const eraser = false;
-const rainbowColor = false;
 
+canvasArea(slider.value);
 
-//slider 
+// rainbow button eventlistener
+
+rainbowButton.addEventListener("click", () => {
+    if (rainbowButton.value == "false") {
+        rainbowButton.value = "true";
+    } else if (rainbowButton.value == "true") {
+        rainbowButton.value = "false";
+    }
+});
+
+//slider event listener
 slider.addEventListener("input",(e) => {
     const b = Number(e.target.value);
 
     //adjusts the size with moving the slider
     canvasArea(b);
     
-})
+});
 
 //reset button eventlistener
 
@@ -67,8 +76,19 @@ function canvasArea (size) {
 
 canvas.addEventListener("mouseover", (e) => {
     const target = e.target;    
-    if (target.matches(".grid-item")) {
-        target.style["background-color"] = currentColor;
+    
+
+    if (rainbowButton.value == "false") {
+        if (target.matches(".grid-item")) {
+            target.style["background-color"] = currentColor;
+        }
+    };
+    if (rainbowButton.value == "true") {
+        if (target.matches(".grid-item")) {
+            const rainbowColor = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+            const randomNumber = Math.floor(Math.random() * 7);
+            target.style["background-color"] = rainbowColor[randomNumber];
+        }
     }
     
 })
